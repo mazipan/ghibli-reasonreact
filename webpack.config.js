3d09df99f989ece62ed6172f463bb57e4a8b8b87
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-04-22 14:34:12 +0700

    chore: change Precache to Workbox

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,9 @@ const StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
 const ManifestPlugin = require('webpack-manifest-plugin');
 const ShakePlugin = require('webpack-common-shake').Plugin;
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 const rollupPluginNodeResolve = require('rollup-plugin-node-resolve');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const prod = process.env.NODE_ENV == 'production';
 const dev = !prod && process.env.DEV !== '0';
@@ -21,7 +22,7 @@ const prodBaseUrl = 'https://ghibli-reasonreact.netlify.com'
 
 let publicUrl = '';
 let publicPath = '/';
-const templateHtml = prod ? path.join(__dirname, '/public/index-prod.html') : path.join(__dirname, '/public/index.html');
+const templateHtml = path.join(__dirname, '/public/index.html');
 
 const extractHTML = new HtmlWebpackPlugin({
   filename: 'index.html',
@@ -68,16 +69,16 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{loader: 'style-loader'}, {loader: 'css-loader'}],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       useRollup
         ? {
-            test: /\.js$/,
-            loader: 'rollup-loader',
-            options: {
-              plugins: [rollupPluginNodeResolve({module: true})],
-            },
-          }
+          test: /\.js$/,
+          loader: 'rollup-loader',
+          options: {
+            plugins: [rollupPluginNodeResolve({ module: true })],
+          },
+        }
         : null,
     ].filter(Boolean),
   },
@@ -87,26 +88,9 @@ module.exports = {
     tls: 'empty',
   },
   plugins: [
-    // Generate a service worker script that will precache, and keep up to date,
-    // the HTML & assets that are part of the Webpack build.
-    new SWPrecacheWebpackPlugin({
-      // By default, a cache-busting query parameter is appended to requests
-      // used to populate the caches, to ensure the responses are fresh.
-      // If a URL is already hashed by Webpack, then there is no concern
-      // about it being stale, and the cache-busting can be skipped.
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: 'service-worker.js',
-      minify: prod || analyze,
-      navigateFallback: publicUrl + '/index.html',
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-    }),
-
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
-    new ManifestPlugin({
-      fileName: 'asset-manifest.json',
-    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(!dev ? 'production' : null),
@@ -115,49 +99,58 @@ module.exports = {
     }),
     useClosureCompiler
       ? new ClosureCompilerPlugin({
-          compiler: {
-            language_in: 'ECMASCRIPT6',
-            language_out: 'ECMASCRIPT5',
-          },
-          concurrency: 3,
-        })
+        compiler: {
+          language_in: 'ECMASCRIPT6',
+          language_out: 'ECMASCRIPT5',
+        },
+        concurrency: 3,
+      })
       : null,
     prod ? new UglifyJsPlugin() : null,
     analyze
       ? new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              warnings: false,
-            },
-            output: {
-              comments: /^\**!|^ [0-9]+ $|@preserve|@license/,
-            },
-          }
-        })
+        uglifyOptions: {
+          compress: {
+            warnings: false,
+          },
+          output: {
+            comments: /^\**!|^ [0-9]+ $|@preserve|@license/,
+          },
+        }
+      })
       : null,
     true
       ? new CompressionPlugin({
-          // asset: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: /\.(js|css)$/,
-          // threshold: 10240,
-          // minRatio: 0.8,
-        })
+        // asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css)$/,
+        // threshold: 10240,
+        // minRatio: 0.8,
+      })
       : null,
     true
       ? new StatsWriterPlugin({
-          filename: 'stats.json',
-          fields: null,
-          transform: function(data) {
-            data.modules.forEach(function(m) {
-              delete m.source;
-            });
-            delete data.children;
-            return JSON.stringify(data, null, 2);
-          },
-        })
+        filename: 'stats.json',
+        fields: null,
+        transform: function (data) {
+          data.modules.forEach(function (m) {
+            delete m.source;
+          });
+          delete data.children;
+          return JSON.stringify(data, null, 2);
+        },
+      })
       : null,
     useShakePlugin ? new ShakePlugin() : null,
     extractHTML,
+    new GenerateSW({
+      navigateFallback: publicUrl + '/index.html',
+    }),
+    new CopyPlugin([
+      { from: 'public/_redirects', to: '.' }
+    ]),
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+    }),
   ].filter(Boolean),
 };
